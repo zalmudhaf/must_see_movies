9c636f5ae7840412bc79d30a046670b9edb08034
Raghu Betina <raghu@firstdraft.com>
2016-11-29 03:49:22 +0000

    Whitelist columns in Movie for ActiveAdmin

--- app/admin/movie.rb
+++ app/admin/movie.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Movie do
 
+ permit_params :director_id, :title, :year
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
