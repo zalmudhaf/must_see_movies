91d9c59db3fd820bcb8e4ded810ba516506cad6c
Raghu Betina <raghu@firstdraft.com>
2016-11-29 03:48:57 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "directors#index"
   # Routes for the Casting resource:
   # CREATE
   get "/castings/new", :controller => "castings", :action => "new"
